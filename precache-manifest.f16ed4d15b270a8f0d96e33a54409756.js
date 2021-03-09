cb538260d7dd23a3f0d40b80f94e11af2506ce60
aliaksei6777 <aliaksei6777@gmail.com>
2021-03-09 23:37:51 +0300

    Updates

---
+++ precache-manifest.f16ed4d15b270a8f0d96e33a54409756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be7c4a47ffa8782027cb4aee45920a15",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "b5b33f7ad613576ff79d",
+    "url": "/TodoList/static/css/main.3a4114f9.chunk.css"
+  },
+  {
+    "revision": "d8b49394a4ef110c78b8",
+    "url": "/TodoList/static/js/2.0f2e4cd6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/TodoList/static/js/2.0f2e4cd6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5b33f7ad613576ff79d",
+    "url": "/TodoList/static/js/main.cd16a184.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
