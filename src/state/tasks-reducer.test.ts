8d4191b3dc6f4638cf4f1f299b66db45b4da22b6
aliaksei6777 <aliaksei6777@gmail.com>
2021-04-16 11:55:04 +0300

    fix tests

--- src/state/tasks-reducer.test.ts
+++ src/state/tasks-reducer.test.ts
@@ -22,7 +22,7 @@ beforeEach(() => {
 
 test ('correct task should be deleted from correct array', () => {
 
-    const action = removeTaskAC("2", "todolistId2");
+    const action = removeTaskAC("todolistId2","2" );
 
     const endState = tasksReducer(startState, action)
 
@@ -30,20 +30,19 @@ test ('correct task should be deleted from correct array', () => {
     expect (endState["todolistId2"].length).toBe(2)
     expect (endState["todolistId2"][0].id).toBe("1")
     expect (endState["todolistId2"][1].id).toBe("3")
-    // expect (endState["todolistId2"].every(t => t.id != "2")).toBeTruthy()
 
 })
 
 test ('correct task should be added to correct array', () => {
 
-    const action = addTaskAC("juce","todolistId2");
+    const action = addTaskAC("todolistId2","juce");
     const endState = tasksReducer(startState, action)
 
     expect (endState["todolistId1"].length).toBe(3)
     expect (endState["todolistId2"].length).toBe(4)
-    expect (endState["todolistId2"][0].id).toBeDefined()
-    expect (endState["todolistId2"][0].title).toBe("juce")
-    expect (endState["todolistId2"][0].isDone).toBe(false)
+    expect (endState["todolistId2"][3].id).toBeDefined()
+    expect (endState["todolistId2"][3].title).toBe("juce")
+    expect (endState["todolistId2"][3].isDone).toBe(false)
 
 })
 
@@ -58,7 +57,7 @@ test ('status of specified task should be changed', () => {
 
 test ('title of specified task should be changed', () => {
 
-    const action = changeTaskTitleAC("2", "Milkyway","todolistId2");
+    const action = changeTaskTitleAC("todolistId2", "2","Milkyway");
     const endState = tasksReducer(startState, action)
 
     expect (endState["todolistId2"][1].title).toBe("Milkyway")
@@ -80,7 +79,7 @@ test ('new property (array) should be added with new todolist', () => {
     expect(endState[newKey]).toEqual([]);
 })
 
-test ('propperty with todolistId should be deleted', () => {
+test ('property with todolistId should be deleted', () => {
 
     const action = removeTodoListAC("todolistId2");
 
