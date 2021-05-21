d3cc74beda25c50b202106362a1ef48c7ce6c024
aliaksei6777 <aliaksei6777@gmail.com>
2021-05-21 22:27:47 +0300

    HashRouter -Switch

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -17,7 +17,7 @@ import {AppRootStateType} from "./store";
 import {initializeAppTC, RequestStatusType} from "./app-reducer";
 import {ErrorSnackbar} from "../components/ ErrorSnackbar/ErrorSnackbar";
 import {Login} from "../features/Login/Login";
-import {HashRouter, Redirect, Route, Switch} from 'react-router-dom';
+import {BrowserRouter, Redirect, Route, Switch} from 'react-router-dom';
 import {logoutTC} from "../features/Login/auth-reducer";
 
 type PropsType = {
@@ -32,13 +32,13 @@ function App({demo = false}: PropsType) {
 
     useEffect(() => {
         dispatch(initializeAppTC())
-    }, [])
+    },[])
 
     const status = useSelector<AppRootStateType, RequestStatusType>(state => state.app.status)
 
     const logoutHandler = useCallback(() => {
         dispatch(logoutTC())
-    }, [])
+    },[])
 
     if (!isInitialized) {
         return <div
@@ -48,7 +48,7 @@ function App({demo = false}: PropsType) {
     }
 
     return (
-        <HashRouter>
+        <BrowserRouter>
             <div className="App">
                 <AppBar position="static">
                     <Toolbar>
@@ -60,17 +60,19 @@ function App({demo = false}: PropsType) {
                         {isLoggedIn && <Button color="inherit" onClick={logoutHandler}>Log out</Button>}
                     </Toolbar>
                 </AppBar>
-                {status === "loading" && <LinearProgress color="secondary"/>}
+                {status === "loading" && <LinearProgress color="secondary" />}
                 <Container fixed>
-                    <Route exact path={'/'} render={() => <TodolistsList demo={demo}/>}/>
-                    <Route path={'/login'} render={() => <Login/>}/>
-                    <Route path={'/404'}
-                           render={() => <h1 style={{textAlign: 'center', fontSize: '50px'}}>404: PAGE NOT FOUND</h1>}/>
-                    <Redirect from={'*'} to={'/404'}/>
+                    <Switch>
+                        <Route exact path={'/TodoList'} render={() => <Redirect to={'/'}/>}/>
+                        <Route exact path={'/'} render={ () => <TodolistsList demo={demo}/> }/>
+                        <Route path={'/login'} render={ () => <Login/> }/>
+                        <Route path={ '/404' } render={ () => <h1 style={{textAlign: 'center',fontSize: '50px'}}>404: PAGE NOT FOUND</h1> }/>
+                        <Redirect from={'*'} to={'/404'}/>
+                    </Switch>
                 </Container>
                 <ErrorSnackbar/>
             </div>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
